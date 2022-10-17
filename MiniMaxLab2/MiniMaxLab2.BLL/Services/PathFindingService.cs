10cf809c756aa0011f41694121d7980544c6d45d
Oleksandr <OlexandrTanasienko@gmail.com>
2022-10-18 02:31:10 +0300

    Remove Lee algorithm

--- MiniMaxLab2/MiniMaxLab2.BLL/Services/PathFindingService.cs
+++ MiniMaxLab2/MiniMaxLab2.BLL/Services/PathFindingService.cs
@@ -45,37 +45,4 @@ public class PathFindingService
 
         return (Array.Empty<int>(), 0);
     }
-    
-    public (int[], int) LeeAlgorithm(int[,] adjacencyMatrix, int[,] fieldMatrix, int start, int end)
-    {
-        var numberOfPoints = adjacencyMatrix.GetLength(0);
-        var queue = new Queue<int>();
-        var from = Enumerable.Repeat(-1, numberOfPoints).ToArray();
-        var visited = Enumerable.Repeat(false, numberOfPoints).ToArray();
-        var distances = Enumerable.Repeat(int.MaxValue, numberOfPoints).ToArray();
-
-        visited[start] = true;
-        distances[start] = 0;
-        queue.Enqueue(start);
-        while (queue.Count != 0)
-        {
-            var current = queue.Dequeue();
-            if (current == end)
-            {
-                return (from, distances[end]);
-            }
-            for (var i = 0; i < numberOfPoints; i++)
-            {
-                if (adjacencyMatrix[current, i] != 0 && !visited[i])
-                {
-                    queue.Enqueue(i);
-                    from[i] = current;
-                    distances[i] = distances[current] + FieldService.DistanceBetweenPoints(current, i, fieldMatrix.GetLength(1));
-                    visited[i] = true;
-                }
-            }
-        }
-
-        return (Array.Empty<int>(), 0);
-    }
 }
