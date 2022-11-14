db03c5f32b26b725fb6d895a324c4c7dcfdea5c5
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-14 14:19:01 +0200

    Create menu class

---
+++ Lab4/Lab4/Menu.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab4;
+
+public static class Menu
+{
+    public static void RunKarpRabinAlgorithm()
+    {
+        Console.WriteLine("Please, enter row of text: ");
+        var text = Console.ReadLine();
+        Console.WriteLine("Please, enter text to find: ");
+        var textToFind = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(textToFind))
+        {
+            Console.WriteLine("Wrong entered input text");
+            return;
+        }
+        KarpRabin.GetIndexesOfFoundSubstring(text, textToFind);
+    }
+}
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,14 +1,4 @@
 ﻿using Lab4;
 
-Console.WriteLine("Please, enter row of text: ");
-var text = Console.ReadLine();
-Console.WriteLine("Please, enter text to find: ");
-var textToFind = Console.ReadLine();
-
-if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(textToFind))
-{
-    Console.WriteLine("Wrong entered input text");
-    return;
-}
-KarpRabin.GetIndexesOfFoundSubstring(text, textToFind);
+Menu.RunKarpRabinAlgorithm();
 
