ef7a894d5f283161358c9d2111c9f85595747ebf
Oleksandr <OlexandrTanasienko@gmail.com>
2022-12-09 21:00:11 +0200

    Create empty project, add objective function

---
+++ NelderMead/NelderMead/NelderMeadMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace NelderMead;
+
+public static class NelderMeadMethod
+{
+    public static int ObjectiveFunction((int x1, int x2, int x3) point)
+        => -5 * point.x1 * (int)Math.Pow(point.x2, 2) * point.x3
+           + 2 * (int)Math.Pow(point.x1, 2) * point.x2
+           - 3 * point.x1 * (int)Math.Pow(point.x2, 4)
+           + point.x1 * (int)Math.Pow(point.x3, 2);
+}
